65adf214b2d4e2609afcaba2cfd37974f476d0d2
Neto Concon <concon.neto2@gmail.com>
2020-12-06 12:37:43 -0300

    finished exercise

--- lib/main.dart
+++ lib/main.dart
@@ -3,53 +3,93 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "People Counter",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget> [
-        Text(
-          "People: 1",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlatButton(
-              child: Text(
-                "-1", 
-                style: TextStyle(
-                  fontSize: 40.0, 
-                  color: Colors.white,)),
-              onPressed: () {},
-            ),  
-            FlatButton(
-              child: Text(
-                "+1", 
-                style: TextStyle(
-                  fontSize: 40.0, 
-                  color: Colors.white,)),
-              onPressed: () {},
-            ),  
-          ]
-        ),
-        Text(
-          "Can enter",
-          style: TextStyle(
-            color: Colors.white, 
-            fontStyle: FontStyle.italic,
-            fontSize: 30.0,
-            ),
-        )
-      ],
-    )
+    home: Home()
   ));
 }
 
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-        // FlatButton(
-        //   child: Text(
-        //     "+1", 
-        //     style: TextStyle(
-        //       fontSize: 40.0, 
-        //       color: Colors.white,))
-        //   onPressed: () {},
-        // ),  
+
+class _HomeState extends State<Home> {
+
+  int _people = 0;
+  String _infoText = "Can enter";
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+
+      if (_people < 0) {
+        _infoText = "Upside down";
+      } else if (_people <= 10) {
+        _infoText = "Can enter";
+      } else {
+        _infoText = "Full";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            Text(
+              "People: $_people",
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1", 
+                      style: TextStyle(
+                        fontSize: 40.0, 
+                        color: Colors.white,)),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                  ), 
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1", 
+                      style: TextStyle(
+                        fontSize: 40.0, 
+                        color: Colors.white,)),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ), 
+                ),
+              ]
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                color: Colors.white, 
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0,
+                ),
+            )
+          ],
+        )
+      ]
+    );
+  }
+}
