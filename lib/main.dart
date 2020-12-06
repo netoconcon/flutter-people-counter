a671d81e5ee7848df92c4a38b0f2466a24986540
Neto Concon <concon.neto2@gmail.com>
2020-12-06 00:02:14 -0300

    flutter basics

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "People Counter",
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget> [
+        Text(
+          "People: 1",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text(
+                "-1", 
+                style: TextStyle(
+                  fontSize: 40.0, 
+                  color: Colors.white,)),
+              onPressed: () {},
+            ),  
+            FlatButton(
+              child: Text(
+                "+1", 
+                style: TextStyle(
+                  fontSize: 40.0, 
+                  color: Colors.white,)),
+              onPressed: () {},
+            ),  
+          ]
+        ),
+        Text(
+          "Can enter",
+          style: TextStyle(
+            color: Colors.white, 
+            fontStyle: FontStyle.italic,
+            fontSize: 30.0,
+            ),
+        )
+      ],
+    )
+  ));
+}
+
+
+        // FlatButton(
+        //   child: Text(
+        //     "+1", 
+        //     style: TextStyle(
+        //       fontSize: 40.0, 
+        //       color: Colors.white,))
+        //   onPressed: () {},
+        // ),  
